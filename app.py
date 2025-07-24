f176b17e4cce8a864a6b116e7d001d060da04766
Dr-Alb <albsipoi1564@gmail.com>
2025-07-24 16:17:02 -0700

    🔧 Updated Render config to include frontend build

--- app.py
+++ app.py
@@ -398,6 +398,8 @@ def create_tables_once():
         db.create_all()
         app.db_initialized = True
 
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     app.run(debug=True)
